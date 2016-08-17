4055e2f3fe5d949265c59d070d23fa2680b4aa03
Gokul Murali <gokul@mallow-tech.com>
2016-08-17 12:24:10 +0530

    corrected a mistake in generator..

--- lib/generators/jwt_authentication/controllers_generator.rb
+++ lib/generators/jwt_authentication/controllers_generator.rb
@@ -1,23 +1,21 @@
 require 'rails/generators'
 
-module Rails
-  module JwtAuthentication
-    class ControllersGenerator < Rails::Generators::Base
-      CONTROLLERS = %w(profile registration session)
+module JwtAuthentication
+  class ControllersGenerator < Rails::Generators::Base
+    CONTROLLERS = %w(profile registration session)
 
-      source_root File.expand_path("../../templates/controllers", __FILE__)
+    source_root File.expand_path("../../templates/controllers", __FILE__)
 
-      class_option :controllers, aliases: "-c", type: :array,
-                   desc: "Select specific controllers to generate (#{CONTROLLERS.join(', ')})"
+    class_option :controllers, aliases: "-c", type: :array,
+                 desc: "Select specific controllers to generate (#{CONTROLLERS.join(', ')})"
 
-      def create_controllers
-        controllers = options[:controllers] || CONTROLLERS
-        controllers.each do |name|
-          template "#{name}_controller.rb",
-                   "app/controllers/#{name}_controller.rb"
-        end
+    def create_controllers
+      controllers = options[:controllers] || CONTROLLERS
+      controllers.each do |name|
+        template "#{name}_controller.rb",
+                 "app/controllers/#{name}_controller.rb"
       end
-
     end
+
   end
 end
